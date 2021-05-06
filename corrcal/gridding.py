63380d7a7c7f998b99fcbcec9457529fad3f61ed
Bobby Pascua <r.pascua@berkeley.edu>
2021-05-05 20:40:27 -0700

    update: initialize gridding module

---
+++ corrcal/gridding.py
@@ -0,0 +1,21 @@
+"""
+Module containing various gridding functions.
+"""
+import numpy as np
+try:
+    import hera_cal
+    HERA_CAL = True
+except ImportError, FileNotFoundError as err:
+    if issubclass(err, ImportError):
+        missing = "hera_cal"
+    else:
+        missing = "git"
+    print(f"{missing} is not installed. Some gridding features unavailable.")
+    HERA_CAL = False
+
+try:
+    import pyfof
+    PYFOF = True
+except ImportError:
+    print("pyfof is not installed. Some gridding features unavailable.")
+    PYFOF = False
