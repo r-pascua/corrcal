4cf25a21972c6b37bc84f6bc9ead3ed4733dc261
Bobby Pascua <r.pascua@berkeley.edu>
2021-06-22 23:09:41 -0700

    update: port over chisq gradient func

--- corrcal/optimize.py
+++ corrcal/optimize.py
@@ -112,3 +112,101 @@ def get_chisq_dense(gains, data, noise, sky_cov, ant1, ant2, scale=1, norm=1):
         np.sum(gains[1::2]) ** 2 + (np.sum(gains[::2]) - gains.size // 2) ** 2
     )
     return raw_chisq + norm
+
+
+def chisq_gradient(gains, data, cov, ant1, ant2, scale=1, norm=1):
+    """Calculate the gradient of chi-squared.
+
+    Parameters
+    ----------
+    Same as chi-squared params. Update this later.
+
+    Returns
+    -------
+    gradient: np.ndarray
+        Gradient of chi-squared with respect to antenna gains.
+    """
+    gains /= scale
+    cov = cov.copy()
+    cov.apply_gains(gains, ant1, ant2)
+    inverse_cov = cov.inverse()
+    # TODO: figure out better names for this... just work out the math
+    # then figure out physically meaningful ways to talk about it.
+    sd = inverse_cov * data
+    gsd = sd.copy()
+    cfuncs.apply_gains_to_matrix(
+        gsd.ctypes.data,
+        gains.ctypes.data,
+        ant2.ctypes.data,
+        ant1.ctypes.data,
+        gsd.size // 2,
+        1,
+    )
+    # This is a little bit of a hack to save some memory.
+    cgsd = cov.copy()
+    cgsd.noise_variance = np.zeros_like(cgsd.noise_variance)
+    cgsd *= gsd
+
+    Nant = max([ant1.max(), ant2.max()]) + 1
+    gradient = np.zeros(2 * Nant, dtype=float)
+
+    # Extract the gains for ease of reference.
+    re_g1 = gains[2 * ant1]
+    re_g2 = gains[2 * ant2]
+    im_g1 = gains[2 * ant1 + 1]
+    im_g2 = gains[2 * ant2 + 1]
+
+    # This is a term in the gradient... Make the code nicer.
+    # This is a bit faster than Jon's implementation, especially
+    # for large arrays.
+    v1_m1r_v2 = np.empty_like(cgsd)
+    v1_m1i_v2 = np.empty_like(cgsd)
+    v1_m2r_v2 = np.empty_like(cgsd)
+    v1_m2i_v2 = np.empty_like(cgsd)
+
+    # Real/imaginary slices for notational convenience/clarity.
+    re = slice(0, None, 2)  # Equivalent to [0::2]
+    im = slice(1, None, 2)  # Equivalent to [1::2]
+
+    # Populate the vectors previously initialized.
+    v1_m1r_v2[re] = re_g2 * sd[re] - im_g2 * sd[im]
+    v1_m1r_v2[im] = im_g2 * sd[re] + re_g2 * sd[im]
+    v1_m1i_v2[re] = im_g2 * sd[re] + re_g2 * sd[im]
+    v1_m1i_v2[im] = -re_g2 * sd[re] + im_g2 * sd[im]
+
+    v1_m2r_v2[re] = re_g1 * sd[re] + im_g1 * sd[im]
+    v1_m2r_v2[im] = -im_g1 * sd[re] + re_g1 * sd[im]
+    v1_m2i_v2[re] = im_g1 * sd[re] - re_g1 * sd[im]
+    v1_m2i_v2[im] = re_g1 * sd[re] + im_g1 * sd[im]
+
+    # Unpack this math to make it make sense.
+    v1_m1r_v2 *= cgsd
+    v1_m1i_v2 *= cgsd
+    v1_m2r_v2 *= cgsd
+    v1_m2i_v2 *= cgsd
+
+    # Sum the real and imaginary parts for some reason?
+    v1_m1r_v2 = v1_m1r_v2[re] + v1_m1r_v2[im]
+    v1_m1i_v2 = v1_m1i_v2[re] + v1_m1i_v2[im]
+    v1_m2r_v2 = v1_m2r_v2[re] + v1_m2r_v2[im]
+    v1_m2i_v2 = v1_m2i_v2[re] + v1_m2i_v2[im]
+
+    cfuncs.sum_grads_c(
+        gradient.ctypes.data,
+        v1_m1r_v2.ctypes.data,
+        v1_m1i_v2.ctypes.data,
+        ant1.ctypes.data,
+        v1_m2i_v2.size,
+    )
+    cfuncs.sum_grads_c(
+        gradient.ctypes.data,
+        v1_m2r_v2.ctypes.data,
+        v1_m2i_v2.ctypes.data,
+        ant2.ctyptes.data,
+        v1_m2i_v2.size,
+    )
+
+    # Finish calculating the gradient.
+    grad_real = 2 * (np.sum(gains[re]) - gains.size / 2) / (gains.size / 2)
+    grad_im = 2 * np.sum(gains[im])
+    return -2 * gradient / scale + norm * (grad_real + grad_im) / scale
