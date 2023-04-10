8d5e0c372b874e5624c2e6c6c82513ddb888bce0
r-pascua <r.pascua@berkeley.edu>
2023-04-10 12:47:39 -0400

    c-tools: add more functions

--- corrcal/src/c_funcs.c
+++ corrcal/src/c_funcs.c
@@ -67,6 +67,14 @@ void woodbury(
      *  k
      *      Number of columns in the U matrix.
      */
+    
+}
+
+
+void matmul(complex *left, complex *right, complex *out, int a, int b, int c) {
+    /*
+     *
+     */
 
 }
 
@@ -90,12 +98,86 @@ void tril_inv(complex *mat, complex *out, int n) {
 }
 
 
+void many_tril_inv(
+    complex *blocks, complex *out, int block_size, int n_blocks
+) {
+    /*
+     *  void many_tril_inv(
+     *      complex *blocks, complex *out, int block_size, int n_blocks
+     *  )
+     *
+     *  Invert many lower triangular matrices of the same shape in parallel.
+     */
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        int offset = i*block_size*block_size;
+        tril_inv(blocks+offset, out+offset, block_size);
+    }
+}
+
+
+void many_irregular_tril_inv(
+    complex *blocks, complex *out, int *block_sizes, int n_blocks
+) {
+    /*
+     *  void many_irregular_tril_inv(
+     *      complex *blocks, complex *out, int *block_sizes, int n_blocks
+     *  )
+     *
+     *  Compute the inverse of a collection of lower-triangular matrices of
+     *  varying sizes.
+     *
+     *  Parameters
+     *  ----------
+     *  blocks
+     *      Matrices to invert.
+     *  out
+     *      Where to write the inverses.
+     *  block_sizes
+     *      Size of each matrix to invert.
+     *  n_blocks
+     *      Number of matrices to invert.
+     */
+    int offsets[n_blocks];
+    int offset = 0;
+    for (int i=0; i<n_blocks; i++) {
+        offsets[i] = offset;
+        offset += block_sizes[i] * block_sizes[i];
+    }
+
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        tril_inv(blocks+offsets[i], out+offsets[i], block_sizes[i]);
+    }
+}
+
+
 void cholesky(complex *mat, complex *out, int n) {
     /*
      *  void cholesky(complex *mat, complex *out, int n)
      *
      *  Compute the Cholesky decomposition of an (n,n) Hermitian matrix.
      */
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) {
+            complex tmp = 0;
+            for (int k=0; k<j; k++) {
+                tmp += out[i*n+k] * conj(out[j*n+k]);
+            }
+            if (i == j) {
+                out[i*n+i] = sqrt(mat[i*n+i] - tmp);
+            } else {
+                out[i*n+j] = (mat[i*n+j] - tmp) / out[j*n+j];
+            }
+        }
+    }
+
+    // Zero out the upper-triangular section
+    for (int i=0; i<n; i++) {
+        for (int j=i+1; j<n; j++) {
+            out[i*n+j] = 0;
+        }
+    }
 }
 
 
@@ -105,4 +187,129 @@ void cholesky_inplace(complex *mat, int n) {
      *
      *  Compute the Cholesky decomposition of an (n,n) matrix in-place.
      */
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++) {
+            complex tmp = 0;
+            for (int k=0; k<j; k++) {
+                tmp += mat[i*n+k] * conj(mat[j*n+k]);
+            }
+            if (i == j) {
+                mat[i*n+i] = sqrt(mat[i*n+i] - tmp);
+            } else {
+                mat[i*n+j] = (mat[i*n+j] - tmp) / mat[j*n+j];
+            }
+        }
+    }
+
+    // Zero out the upper-triangular section.
+    for (int i=0; i<n; i++) {
+        for (int j=i+1; j<n; j++) {
+            mat[i*n+j] = 0;
+        }
+    }
+}
+
+
+void many_chol(
+    complex *blocks, complex *out, int block_size, int n_blocks
+) {
+    /*
+     *  void many_chol(
+     *      complex *blocks, complex *out, int block_size, int n_blocks
+     *  )
+     *
+     *  Calculate the Cholesky decomposition of a collection of regularly
+     *  sized matrices in parallel.
+     */
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        int offset = i * block_size * block_size;
+        cholesky(blocks+offset, out+offset, block_size);
+    }
+}
+
+
+void many_chol_inplace(complex *blocks, int block_size, int n_blocks) {
+    /*
+     *  void many_chol_inplace(
+     *      complex *blocks, int block_size, int n_blocks
+     *  )
+     *
+     *  Calculate the Cholesky decomposition of a collection of regularly
+     *  sized matrices in-place, in parallel.
+     */
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        int offset = i * block_size * block_size;
+        cholesky_inplace(blocks+offset, block_size);
+    }
+}
+
+
+void many_irregular_chol(
+    complex *blocks, complex *out, int *block_sizes, int n_blocks
+) {
+    /*
+     *  void many_irregular_chol(
+     *      complex *blocks, complex *out, int *block_sizes, int n_blocks
+     *  )
+     *
+     *  Calculate the Cholesky decomposition of a collection of matrices of
+     *  varying sizes.
+     *
+     *  Parameters
+     *  ----------
+     *  blocks
+     *      Array of matrices.
+     *  out
+     *      Where to write the Cholesky decomposition of the input matrices.
+     *  block_sizes
+     *      Size of each matrix.
+     *  n_blocks
+     *      Number of matrices.
+     */ 
+    int offsets[n_blocks];
+    int offset = 0;
+    for (int i=0; i<n_blocks; i++) {
+        offsets[i] = offset;
+        offset += block_sizes[i] * block_sizes[i];
+    }
+
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        cholesky(blocks+offsets[i], out+offsets[i], block_sizes[i]);
+    }
+}
+
+
+void many_irregular_chol_inplace(
+    complex *blocks, int *block_sizes, int n_blocks
+) {
+    /*
+     *  void many_irregular_chol_inplace(
+     *      complex *blocks, int *block_sizes, int n_blocks
+     *  )
+     *
+     *  Calculate the Cholesky decomposition of a list of matrices in-place.
+     *
+     *  Parameters
+     *  ----------
+     *  blocks
+     *      Array of matrices.
+     *  block_sizes
+     *      Size of each matrix. Note that these are 32-bit integers.
+     *  n_blocks
+     *      Number of matrices.
+     */
+    int offsets[n_blocks];
+    int offset = 0;
+    for (int i=0; i<n_blocks; i++) {
+        offsets[i] = offset;
+        offset += block_sizes[i] * block_sizes[i];
+    }
+
+    #pragma omp parallel for
+    for (int i=0; i<n_blocks; i++) {
+        cholesky_inplace(blocks+offsets[i], block_sizes[i]);
+    }
 }
