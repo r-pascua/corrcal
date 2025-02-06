e8c359596c4d09ad57cc5d8137e46f8b121c94ce
r-pascua <r.pascua@berkeley.edu>
2025-02-06 00:56:21 -0500

    test: start porting over some tests

--- corrcal/tests/test_linalg.py
+++ corrcal/tests/test_linalg.py
@@ -2,59 +2,29 @@ import pytest
 import numpy as np
 from corrcal import linalg
 
-def mock_data(shape):
-    return np.random.normal(size=shape) + 1j*np.random.normal(size=shape)
+def test_make_small_blocks(noise, diff_mat, dense_diff_mat, edges, n_eig):
+    small_blocks = linalg.make_small_blocks(
+        noise_diag=noise, diff_mat=diff_mat, edges=edges
+    )
+    dense_small_blocks = dense_diff_mat.T @ np.diag(1/noise) @ dense_diff_mat
+    blocks = np.zeros((edges.size-1, 2*n_eig, 2*n_eig), dtype=float)
+    for grp in range(edges.size-1):
+        sl = slice(2*grp*n_eig, 2*(grp+1)*n_eig)
+        blocks[grp] = dense_small_blocks[sl,sl]
+    assert np.allclose(blocks, small_blocks)
+
+
+def test_tril_inv(noise, diff_mat, edges):
+    blocks = np.eye(diff_mat.shape[1])[None,...] + linalg.make_small_blocks(
+        noise_diag=noise, diff_mat=diff_mat, edges=edges
+    )
+    blocks = np.linalg.cholesky(blocks)
+    inv_blocks = np.linalg.inv(blocks)
+    assert np.allclose(linalg.tril_inv(blocks), inv_blocks)
+
+
+def test_block_multiply(diff_mat, dense_diff_mat, edges, n_eig):
+    small_blocks = linalg.make_small_blocks(
+        noise_diag=noise, diff_mat=diff_mat, edges=edges
+    )
     
-
-@pytest.fixture
-def edges():
-    return np.array([0,3,5,9,15]).astype(int)
-
-
-@pytest.fixture
-def n_bl():
-    return 50
-
-
-@pytest.fixture
-def n_eig():
-    return 3
-
-
-@pytest.fixture
-def n_src():
-    return 10
-
-
-@pytest.fixture
-def diff_mat(n_bl, n_eig):
-    return mock_data((n_bl, n_eig))
-
-
-@pytest.fixture
-def src_mat(n_bl, n_src):
-    return mock_data((n_bl, n_src))
-
-
-@pytest.fixture
-def noise(n_bl):
-    return np.abs(mock_data(n_bl)).astype(complex)
-
-
-def test_make_small_blocks(edges, n_bl, n_eig, noise, diff_mat):
-    n_grp = edges.size - 1
-    dense_diff_mat = np.zeros((n_bl, n_eig*n_grp), dtype=complex)
-    for grp in range(n_grp):
-        bl_slice = slice(edges[grp], edges[grp+1])
-        eig_slice = slice(grp*n_eig, (grp+1)*n_eig)
-        dense_diff_mat[bl_slice,eig_slice] = diff_mat[bl_slice]
-
-    N_inv = np.diag(1/noise)
-    answer = dense_diff_mat.T.conj() @ N_inv @ dense_diff_mat
-    sparse_product = linalg.make_small_blocks(noise, diff_mat, edges)
-    dense_product = np.zeros((n_eig*n_grp, n_eig*n_grp), dtype=complex)
-    for grp in range(n_grp):
-        sl = slice(grp*n_eig, (grp+1)*n_eig)
-        dense_product[sl,sl] = sparse_product[grp]
-    
-    assert np.allclose(dense_product, answer)
--- corrcal/tests/test_sparse.py
+++ corrcal/tests/test_sparse.py
@@ -1,5 +1,39 @@
 import pytest
+import numpy as np
+from corrcal import linalg
 
 
-def test_placeholder():
-    pass
+def test_compute_inv_diff_mat(diff_mat, dense_diff_mat, noise, edges):
+    """Validate the sparse calculation of the "inverse" diffuse matrix."""
+    n_eig = diff_mat.shape[1]
+    N_inv = 1 / noise
+
+    # Compute the inverse diffuse matrix as it's done in SparseCov.inv
+    small_blocks = linalg.make_small_blocks(
+        noise_diag=noise, diff_mat=diff_mat, edges=edges
+    )
+    small_blocks = np.eye(n_eig)[None,...] + small_blocks
+    small_blocks = np.linalg.cholesky(small_blocks)
+    small_inv = linalg.tril_inv(small_blocks).transpose(0,2,1).copy()
+    tmp = N_inv[:,None] * diff_mat
+    inv_diff_mat = linalg.block_multiply(tmp, small_inv, edges)
+    dense_inv_diff_mat = np.zeros_like(dense_diff_mat)
+    for grp, (start, stop) in enumerate(zip(edges, edges[1:])):
+        sl = slice(grp*n_eig, (grp+1)*n_eig)
+        dense_inv_diff_mat[start:stop,sl] = inv_diff_mat[start:stop]
+    
+    # Now compute it manually.
+    dense_small_blocks = np.eye(
+        n_eig * (edges.size-1)
+    ) + dense_diff_mat.T @ (N_inv[:,None] * dense_diff_mat)
+    inv_diff_mat = N_inv[:,None] * (
+        dense_diff_mat @ np.linalg.inv(np.linalg.cholesky(dense_small_blocks)).T
+    )
+    assert np.allclose(dense_inv_diff_mat, inv_diff_mat)
+
+
+def test_inv(cov):
+    assert np.allclose(np.linalg.inv(cov.expand()), cov.inv().expand())
+
+def test_double_inversion(cov):
+    assert np.allclose(cov.expand(), cov.inv().inv().expand())
