fix: normalization for diffuse matrix calculation
Bandpowers in diffuse matrix calculation contained a factor of 4pi that
shouldn't have been there. Probably something to do with healpy's
convention for normalizing the spherical harmonics disagreeing with the
convention I used when doing the math for the diffuse matrix
calculation.

corrcal/models.py
@@ -489,9 +489,7 @@ def _compute_diffuse_matrix_from_flat_sky(
         # Compute the bandpower measured by this group.
         avg_uvw_mag = np.linalg.norm(uvw_here.mean(axis=0))
         pspec_bessels = spherical_jn(sky_ells, 2*np.pi*avg_uvw_mag)
-        band_power = 4 * np.pi * np.sum(
-            sky_pspec * (2*sky_ells + 1) * pspec_bessels**2
-        )
+        band_power = np.sum(sky_pspec * (2*sky_ells + 1) * pspec_bessels**2)
 
         # Compute the fringe harmonics.
         bessels = spherical_jn(ells, 2*np.pi*uvw_diff_mags[None,:,:])
@@ -514,9 +512,35 @@ def _compute_diffuse_matrix_from_flat_sky(
 
         # Now take the eigendecomposition.
         eigvals, eigvecs = np.linalg.eigh(block)
-        sort = np.argsort(eigvals)[::-1]
-        eigvals = eigvals[sort][:2*n_eig]
-        eigvecs = eigvecs[:,sort][:,:2*n_eig]
+
+        # eigh returns in ascending eigenvalue order; we  want the biggest.
+        eigvals = eigvals[::-1][:2*n_eig]
+
+        # Need to sort the real/imag separately to maintain parity.
+        _eigvecs = eigvecs.copy()
+        _eigvecs[:,::2] = eigvecs[:,::2][:,::-1]
+        _eigvecs[:,1::2] = eigvecs[:,1::2][:,::-1]
+        eigvecs = _eigvecs.copy()[:,:2*n_eig]
+
+        # We want each 2x2 block to look like the covariance of a complex
+        # number, but sometimes the eigenvector order is swapped. For a GRF,
+        # the diagonal should be larger than the off-diagonal, so we ensure
+        # that is always the case. It suffices to check the first two modes,
+        # because these roughly represent the redundantly averaged visibility.
+        if np.abs(eigvecs[0,0]) < np.abs(eigvecs[1,0]):
+            _eigvecs = eigvecs.copy()
+            _eigvecs[::2,::2] = eigvecs[1::2,::2]
+            _eigvecs[1::2,::2] = eigvecs[::2,::2]
+            eigvecs = _eigvecs.copy()
+
+        if np.abs(eigvecs[1,1]) < np.abs(eigvecs[0,1]):
+            _eigvecs = eigvecs.copy()
+            _eigvecs[::2,1::2] = eigvecs[1::2,1::2]
+            _eigvecs[1::2,1::2] = eigvecs[::2,1::2]
+            eigvecs = _eigvecs.copy()
+
+        # This block of the diffuse matrix is just the truncated
+        # eigendecomposition of the expected covariance for this block.
         block = np.sqrt(eigvals)[None,:] * eigvecs
         diff_mat[start:stop] = np.where(np.isnan(block), 0, block)
 